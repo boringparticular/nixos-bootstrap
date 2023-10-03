cba6bd042e09b9f50158534ddee0c1517bc23ac7
Kai Mies <develop@regnavi.de>
2023-10-03 07:50:48 +0200

    Meh

--- bootstrap-gpt.sh
+++ bootstrap-gpt.sh
@@ -6,8 +6,5 @@ sudo parted -- "$DISK" mklabel gpt
 sudo parted -- "$DISK" mkpart ESP fat32 1MiB 512MiB
 sudo parted -- "$DISK" set 1 esp on
 # sudo parted -- "$DISK" set 1 boot on
-sudo parted -- "$DISK" mkpart primary 512MiB 100%
-
-sudo mkfs.fat -F 32 -n UEFI /dev/"$DISK"1
 
 source ./bootstrap.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-sudo mkfs.btrfs -L NIXROOT /dev/"$DISK"2
+sudo parted -- "$DISK" mkpart primary 512MiB 100%
+
+sudo mkfs.fat -F 32 -n boot /dev/"$DISK"1
+sudo mkfs.btrfs -L nixos /dev/"$DISK"2
 
 sudo mount /dev/"$DISK"2 /mnt
 sudo btrfs subvolume create /mnt/root
@@ -19,5 +22,5 @@ sudo mount /dev/"$DISK"1 /mnt/boot
 
 sudo nixos-generate-config --root /mnt
 sudo cp ./minimal.nix /etc/nixos/configuration.nix
-sudo nixos-install --root /mnt
+sudo nixos-install --root /mnt --no-root-passwd
 sudo reboot
