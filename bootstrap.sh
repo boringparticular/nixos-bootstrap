71388a38176c7a4ee08ca9ae7bbb37b9126c89e2
Kai Mies <develop@regnavi.de>
2023-10-03 08:15:59 +0200

    Fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,23 +16,23 @@ fi
 
 sudo parted -- "$DISK" mkpart primary 512MiB 100%
 
-sudo mkfs.fat -F 32 -n boot /dev/"$DISK"1
-sudo mkfs.btrfs -L nixos /dev/"$DISK"2
+sudo mkfs.fat -F 32 -n boot /"$DISK"1
+sudo mkfs.btrfs -L nixos /"$DISK"2
 
-sudo mount /dev/"$DISK"2 /mnt
+sudo mount "$DISK"2 /mnt
 sudo btrfs subvolume create /mnt/root
 sudo btrfs subvolume create /mnt/home
 sudo btrfs subvolume create /mnt/nix
 sudo btrfs subvolume create /mnt/swap
 sudo umount /mnt
 
-sudo mount -o compress=zstd,subvol=root /dev/"$DISK"2 /mnt
+sudo mount -o compress=zstd,subvol=root /"$DISK"2 /mnt
 sudo mkdir /mnt/{home,nix}
-sudo mount -o compress=zstd,subvol=home /dev/"$DISK"2 /mnt/home
-sudo mount -o compress=zstd,subvol=nix /dev/"$DISK"2 /mnt/nix
+sudo mount -o compress=zstd,subvol=home /"$DISK"2 /mnt/home
+sudo mount -o compress=zstd,subvol=nix /"$DISK"2 /mnt/nix
 
 sudo mkdir /mnt/boot
-sudo mount /dev/"$DISK"1 /mnt/boot
+sudo mount "$DISK"1 /mnt/boot
 
 sudo nixos-generate-config --root /mnt
 
